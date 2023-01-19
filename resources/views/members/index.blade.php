4877cf76c7d8f7e7e5ba48540d65bb8687f30bd6
hatanokunsan <t_hatano@b-forme.net>
2023-01-19 15:56:41 +0900

    RESTコントローラを作成

---
+++ resources/views/members/index.blade.php
@@ -0,0 +1,3 @@
+@section('content')
+    <h1>test</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/member');
 });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Route::resource('/member', [App\Http\Controllers\MemberController::class, 'index']);
