2417a0c8480a1a6a802643247bb97b4116245d6c
hatanokunsan <t_hatano@b-forme.net>
2023-01-20 11:20:23 +0900

    Route::resourceを一旦諦める

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\MemberController;
 
 Route::get('/', function () {
     return redirect('/member');
@@ -19,6 +9,9 @@ Route::get('/', function () {
 
 Auth::routes();
 
+// Route::resource('/member', [MemberController::class]);
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-// Route::resource('/member', [App\Http\Controllers\MemberController::class, 'index']);
+Route::get('/member', [MemberController::class, 'index'])->name('member');
+
