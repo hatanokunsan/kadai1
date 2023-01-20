ae8463b7ec05619b3d518cbaec16e1f13b103d0d
hatanokunsan <t_hatano@b-forme.net>
2023-01-20 14:46:28 +0900

    Routeの記述方法を変更

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,10 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/member', [MemberController::class, 'index'])->name('member');
-
+Route::get('/', [MemberController::class, 'index'])->name('member.index');
+Route::get('/create', [MemberController::class, 'create'])->name('member.create');
+Route::post('/store', [MemberController::class, 'store'])->name('member.store');
+Route::get('/show/{id}', [MemberController::class, 'show'])->name('member.show');
+Route::get('/edit/{id}', [MemberController::class, 'edit'])->name('member.edit');
+Route::post('/update{id}', [MemberController::class, 'update'])->name('member.update');
+Route::post('/destroy{id}', [MemberController::class, 'destroy'])->name('member.destroy');
