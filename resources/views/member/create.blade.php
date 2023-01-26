b7f8bd1c8ff400aded21f78a9899f553ec79cb62
hatanokunsan <t_hatano@b-forme.net>
2023-01-26 15:17:49 +0900

    エラーメッセージの表示を実装

--- resources/views/member/create.blade.php
+++ resources/views/member/create.blade.php
@@ -9,27 +9,41 @@
             <li class="list-group-item">
                 <label>name</label>
                 <input class="w-100" type="text" name="name" id="name" value={{ old('name') }}>
+                @error('name')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
             </li>
             <li class="list-group-item">
                 <label>mail</label>
                 <input class="w-100" type="email" name="mail" id="mail" value={{ old('mail') }}>
+                @error('mail')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
             </li>
             <li class="list-group-item">
                 <label>age</label>
                 <input class="w-100" type="number" name="age" id="age" value={{ old('age') }}>
+                @error('age')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
             </li>
             <li class="list-group-item">
                 <label>gender</label>
                 <div class="form-group">
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="gender" id="gender1" value="male">
+                        <input class="form-check-input" type="radio" name="gender" id="gender1" value="male"
+                            {{ old('gender') == 'male' ? 'checked' : '' }}>
                         <label class="form-check-label" for="gender1">male</label>
                     </div>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="gender" id="gender2" value="female">
+                        <input class="form-check-input" type="radio" name="gender" id="gender2" value="female"
+                            {{ old('gender') == 'female' ? 'checked' : '' }}>
                         <label class="form-check-label" for="gender2">female</label>
                     </div>
                 </div>
+                @error('gender')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
             </li>
             <li class="list-group-item">
                 <input class="btn btn-primary btn-sm" type="submit" value="{{ $sub_title }}">
