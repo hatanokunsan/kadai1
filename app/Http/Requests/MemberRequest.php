8c8e01e0072df55d6af6fe5e1380a62e46bb5592
hatanokunsan <t_hatano@b-forme.net>
2023-01-26 14:38:44 +0900

    ルールとメッセージを記述

--- app/Http/Requests/MemberRequest.php
+++ app/Http/Requests/MemberRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class MemberRequest extends FormRequest
 {
@@ -24,7 +25,30 @@ class MemberRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name' => [
+                'required',
+                // 一意である事をチェックするが、自分自身は除外している
+                Rule::unique('members')->ignore($this->id), // ignore(除外対象)
+            ],
+            'mail' => [
+                'required',
+                // 一意である事をチェックするが、自分自身は除外している
+                Rule::unique('members')->ignore($this->id), // ignore(除外対象)
+            ],
+            'age' => 'required',
+            'gender' => 'required',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'nameは必須入力です',
+            'name.unique' => '既に登録されています',
+            'mail.required' => 'mailは必須入力です',
+            'mail.unique' => '既に登録されています',
+            'age.required' => 'ageは必須入力です',
+            'gender.required' => 'genderは必須入力です',
         ];
     }
 }
