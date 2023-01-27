e3d84a13f4426988cc7111cf45e76905e346d7f1
hatanokunsan <t_hatano@b-forme.net>
2023-01-27 11:52:15 +0900

    Route::getを追加

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,6 @@ Route::post('/store', [MemberController::class, 'store'])->name('member.store');
 Route::get('/show/{id}', [MemberController::class, 'show'])->name('member.show');
 Route::get('/edit/{id}', [MemberController::class, 'edit'])->name('member.edit');
 Route::patch('/update/{id}', [MemberController::class, 'update'])->name('member.update');
+// URLの{id}パラメータを変更された時の為にルートを用意した
+Route::get('/destroy/{id}', [MemberController::class, 'destroy'])->name('member.destroy');
 Route::delete('/destroy/{id}', [MemberController::class, 'destroy'])->name('member.destroy');
